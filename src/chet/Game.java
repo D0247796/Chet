41dc60dea0ad57340a56014aedb5ab39632edf55
AlexLeoTW <alex@alexleo.click>
2016-12-25 10:38:32 +0800

    initial commit, based on D0247796/chet e75371b7715

---
+++ src/chet/Game.java
@@ -0,0 +1,20 @@
+package chet;
+
+import java.util.Observable;
+
+public class Game {
+	Rule rule;
+	Chess[] allChess;
+	Player p1,p2;
+	public Game(Chess[] allChess ,Player p1 ,Player p2){
+		this.allChess=allChess;
+		this.p1=p1;
+		this.p2=p2;
+		rule=new Rule(allChess,p1,p2);
+	}
+	
+	public void change(Chess c,Coordinate coo){
+		rule.isMove(c, coo);
+	}
+	
+}
